0008e610390852a1240a104c00bf6dbb6ff5e90d
tdude0175 <tdude0175@gmail.com>
2019-02-05 15:47:57 -0600

    completed problems 1-4 with help, commented challenge requirements

---
+++ tic-tac-toe.js
@@ -0,0 +1,103 @@
+var  spaces = document.querySelectorAll("td");
+var currentPlayer = 1;
+var player = document.getElementById("player");
+var resetButton = document.querySelector("button");
+function reset()
+{
+    for(i=0; i < spaces.length;i++)
+    {
+        spaces[i].innerHTML = "";
+        currentPlayer = 1;
+        player.textContent = ("Player " + currentPlayer);
+    }
+}
+tietest = 0;
+function checkForWinner()
+{
+        if ((spaces[0].innerText === spaces[1].innerText && spaces[1].innerText === spaces[2].innerText && spaces[2].innerText === "X")
+            || (spaces[3].innerText === spaces[4].innerText && spaces[4].innerText === spaces[5].innerText && spaces[5].innerText === "X")
+            || (spaces[6].innerText === spaces[7].innerText && spaces[7].innerText === spaces[8].innerText && spaces[8].innerText === "X")
+            || (spaces[0].innerText === spaces[3].innerText && spaces[3].innerText === spaces[6].innerText && spaces[6].innerText === "X")
+            || (spaces[1].innerText === spaces[4].innerText && spaces[4].innerText === spaces[7].innerText && spaces[7].innerText === "X")
+            || (spaces[2].innerText === spaces[5].innerText && spaces[5].innerText === spaces[8].innerText && spaces[8].innerText === "X")
+            || (spaces[0].innerText === spaces[4].innerText && spaces[4].innerText === spaces[8].innerText && spaces[8].innerText === "X")
+            || (spaces[2].innerText === spaces[4].innerText && spaces[4].innerText === spaces[6].innerText && spaces[6].innerText === "X")) {
+            alert("X wins");
+            player.textContent = ("Player " + (currentPlayer - 1) + " wins");
+            reset();
+        } else if ((spaces[0].innerText === spaces[1].innerText && spaces[1].innerText === spaces[2].innerText && spaces[2].innerText === "O")
+            || (spaces[3].innerText === spaces[4].innerText && spaces[4].innerText === spaces[5].innerText && spaces[5].innerText === "O")
+            || (spaces[6].innerText === spaces[7].innerText && spaces[7].innerText === spaces[8].innerText && spaces[8].innerText === "O")
+            || (spaces[0].innerText === spaces[3].innerText && spaces[3].innerText === spaces[6].innerText && spaces[6].innerText === "O")
+            || (spaces[1].innerText === spaces[4].innerText && spaces[4].innerText === spaces[7].innerText && spaces[7].innerText === "O")
+            || (spaces[2].innerText === spaces[5].innerText && spaces[5].innerText === spaces[8].innerText && spaces[8].innerText === "O")
+            || (spaces[0].innerText === spaces[4].innerText && spaces[4].innerText === spaces[8].innerText && spaces[8].innerText === "O")
+            || (spaces[2].innerText === spaces[4].innerText && spaces[4].innerText === spaces[6].innerText && spaces[6].innerText === "O")) {
+            alert("O wins");
+            player.textContent = ("Player " + (currentPlayer + 1) + " wins");
+            reset();
+        }
+        else if(spaces[0].innerText !== "" &&spaces[1].innerText !== "" && spaces[2].innerText !== ""
+            && spaces[3].innerText !== "" && spaces[4].innerText !== "" && spaces[5].innerText !== ""   //to much typing
+            && spaces[6].innerText !== "" && spaces[7].innerText !== "" &&spaces[8].innerText !== "")   //use a loop?
+        {
+                alert("tie");
+                reset();
+        }
+// if you want to do challenges, build 2 more variables [number of rounds] and [current round]
+// add two more variables called [player1 wins] and [player two wins]
+//set it so if x wins [player1wins++] if o wins [player2++] and then [current round++]
+// when game starts get {prompt} from user for [number of rounds]
+// when [number of rounds] === [current round] if [player1wins > player2wins = player1wins else player2wins]
+//
+}
+
+function clickedSquare(element)
+{
+    if(element.target.innerHTML === "")
+    {
+        if (currentPlayer === 1)
+        {
+            currentPlayer += 1;
+            player.textContent = ("Player " + currentPlayer);
+            element.target.innerHTML = "X";
+        } else if (currentPlayer !== 1) {
+            currentPlayer -= 1;
+            player.textContent = ("Player " + currentPlayer);
+            element.target.innerHTML = "O";
+        }
+
+        /*
+        this code is a test of using a loop for the test
+        array inside an array?
+        // for (i = 0; i < spaces.length; i++)
+        // {
+        //     if(spaces[i+1] === null)
+        //     {
+        //
+        //     }
+        //     else
+        //         {
+        //         if (spaces[i].innerHTML === "X" && spaces[i + 1].innerHTML === "X" && spaces[i + 2].innerHTML === "X")
+        //         {
+        //             alert("player one wins");
+        //         }
+        //         else if (spaces[i].innerHTML === "O" && spaces[i + 1].innerHTML === "O" && spaces[i + 2].innerHTML === "O")
+        //         {
+        //             alert("player two wins");
+        //         }
+        //     }
+        // }
+        */
+    }
+    checkForWinner();
+
+}
+// Add a click event listener for each game space
+for (var i = 0; i < spaces.length; i++)
+{
+    spaces[i].addEventListener('click', clickedSquare);
+}
+
+resetButton.onclick = reset;
+//add a button to reset
